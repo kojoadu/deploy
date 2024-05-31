524c061f30e3642f5771a9eed825c283e6510aa8
kojoadu <kojoadu@live.com>
2024-05-31 18:29:03 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+df = pd.read_csv('superstore.csv', encoding='latin1')
+
+def formatIndex(df):
+    df['S/No.'] = range(1, len(df) + 1)
+    df = df.set_index('S/No.')
+    return df
+
+product_list = df['Product Name'].unique()
+total_sales_product = df[['Product Name', 'Sales']].groupby('Product Name').agg({'Sales':'sum'}).reset_index().sort_values(by='Sales', ascending=False)
+total_sales_product['Sales'] = round(total_sales_product['Sales'],2)
+total_sales_product = formatIndex(total_sales_product)
+st.dataframe(total_sales_product.head(10))
+
+fig = px.bar(total_sales_product, x='Product Name', y='Sales', color='Product Name')
+st.plotly_chart(fig)
